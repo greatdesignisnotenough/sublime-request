629dab83d7dc5235a596a347f24a4cf998fe2f0e
greatdesignisnotenough <pierre.alexandre.schembri@gmail.com>
2013-12-17 00:28:53 +0100

    Update request.py
    Update with error handling : A message is printed when non printable characters are found.

--- request.py
+++ request.py
@@ -14,7 +14,7 @@ except ImportError:
 class RequestCommand(sublime_plugin.WindowCommand):
     def run(self, open_args=[], open_kwargs={},
             read_args=[], read_kwargs={},
-            save_to_clipboard=False):
+            save_to_clipboard=False, insert_in_current_view=False):
         # Make the request as requested
         url = open_args[0] or open_kwargs.get('url', None)
 
@@ -35,19 +35,27 @@ class RequestCommand(sublime_plugin.WindowCommand):
         # Read in the result and update the user
         result = req.read(*read_args, **read_kwargs)
         sublime.status_message('Successfully read from "%s"' % url)
+        
+        # Create a string from the result
+        try:
+            result_string = unicode(result, errors='strict')
+        except UnicodeDecodeError:
+            result_string = u'>>>WARNING: Non printable characters within response<<<\n\n'
+            result_string += unicode(result, errors='ignore')
 
         # If we should save result to clipboard, save it
         if save_to_clipboard:
             # DEV: For Sublime Text 3 support, we must coerce result from bytes into a string
-            sublime.set_clipboard(unicode(result, errors='ignore'))
+            sublime.set_clipboard(result_string)
             sublime.status_message('Saved result from "%s" to clipboard' % url)
         
         # If we want to insert directly the content of the response in the current view
         if insert_in_current_view:
             view = sublime.active_window().active_view()
-            edit = view.begin_edit()
-            for region in view.sel():
-                view.replace(edit, region, unicode(result, errors='ignore'))
+            if view:
+                edit = view.begin_edit()
+                for region in view.sel():
+                    view.replace(edit, region, result_string)
                 view.end_edit(edit)
         
         
