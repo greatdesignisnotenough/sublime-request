a0a26557e519caa41df9aab9d8908d1e72895590
greatdesignisnotenough <pierre.alexandre.schembri@gmail.com>
2013-12-17 15:55:50 +0100

    Specific usage with json objects

--- request.py
+++ request.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # Load in core dependencies
 import sublime
 import sublime_plugin
+import json
 
 # Attempt to load urllib.request/error and fallback to urllib2 (Python 2/3 compat)
 try:
@@ -14,9 +18,25 @@ except ImportError:
 class RequestCommand(sublime_plugin.WindowCommand):
     def run(self, open_args=[], open_kwargs={},
             read_args=[], read_kwargs={},
-            save_to_clipboard=False, insert_in_current_view=False, decode_as='str'):
-        # Make the request as requested
-        url = open_args[0] or open_kwargs.get('url', None)
+            save_to_clipboard=False, insert_in_current_view=False, decode_as='str',
+            selection_request=False, json_key = False):
+        
+        if selection_request:
+            view = sublime.active_window().active_view()
+            if view:
+                selections = view.sel()
+                if len(selections) > 0:
+                    currentRegion = view.word(selections[0])
+                    currentWord = view.substr(currentRegion)
+                    if len(currentWord) > 0:
+                        # Construct url
+                        open_args[0] = open_args[0] + currentWord
+                        url = open_args[0]                  
+                    else:
+                        # Construct url - no extra
+                        url = open_args[0] or open_kwargs.get('url', None)
+        else:
+            url = open_args[0] or open_kwargs.get('url', None)
 
         # Attempt to open the url
         try:
@@ -40,12 +60,20 @@ class RequestCommand(sublime_plugin.WindowCommand):
         decoder = self.decoders[decode_as]
         result_string = decoder(result)
         
+        # Get value from the json response
+        if decode_as == 'json':
+            if json_key in result_string:
+                result_string = result_string[json_key]
+            else:
+                sublime.status_message('No result found for "%s"' % json_key)
+
         # If we should save result to clipboard, save it
         if save_to_clipboard:
             # DEV: For Sublime Text 3 support, we must coerce result from bytes into a string
-            sublime.set_clipboard(result_string)
-            sublime.status_message('Saved result from "%s" to clipboard' % url)
-        
+            if type(result_string) == unicode or type(result_string) == str:
+                sublime.set_clipboard(result_string)
+                sublime.status_message('Saved result from "%s" to clipboard' % url)            
+
         # If we want to insert directly the content of the response in the current view
         if insert_in_current_view:
             view = sublime.active_window().active_view()
@@ -57,7 +85,7 @@ class RequestCommand(sublime_plugin.WindowCommand):
 
     # Define a collection of decoders
     decoders = {}
-    
+   
     @classmethod
     def add_decoder(cls, name, fn):
         cls.decoders[name] = fn
@@ -66,3 +94,4 @@ class RequestCommand(sublime_plugin.WindowCommand):
 RequestCommand.add_decoder('str', str)
 RequestCommand.add_decoder('unicode', unicode)
 RequestCommand.add_decoder('unicode_tolerant', lambda x: unicode(x, errors='ignore'))
+RequestCommand.add_decoder('json', lambda x: json.loads(x))
